4cf1f99ea584f2f6b732d49511fc5ca473c44885
Sana <sanagubari27@gmail.com>
2023-01-09 23:43:31 +0000

    Add More Css Styling so that dropdows have teh same sytling as the rest of page and pages has more white space

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
 import Button from 'react-bootstrap/Button';
+import { Link } from 'react-router-dom';
 
 export const Header = () => {
   return (
     <header className='header'>
+      <Link to='/' className='Link'>
       <h1 className='header-content'>NC GAMES</h1>
+      </Link>
       <Button className='header-content Login-Button' variant="light" >Login</Button>{' '}
     </header>
   );
